5741205565b0a44cb86c4639a2022eb8d241116c
marioc <marioc@ega1.ebi.ac.uk>
2009-07-12 23:11:58 +0100

    count_kmers works here - before sending this to Tom Skelly

--- src/count_kmers/count_kmers.c
+++ src/count_kmers/count_kmers.c
@@ -3,19 +3,29 @@
 #include <open_hash/hash_table.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int main(int argc, char **argv){
   Sequence * seq;
   FILE *fp_fnames,*fp_file;
  
   char filename[100];
+  int max_read_length = 2000;
+  int fastq; //if 0 entry is fasta otherwise is quality cut-off
   int kmer_size, hash_key_bits;
-
+  int bucket_size;
   HashTable * hash_table = NULL;
-   
+  long long entry_length;
+
+  boolean full_entry = true;
+  boolean prev_full_entry = true;
+  Element * prev_entry =  NULL;
+
   long count=0;
   FILE *fout;
   
+
+  //useful functions
   void print_element_count(Element * e){
   
     char filename [50];
@@ -39,27 +49,105 @@ int main(int argc, char **argv){
   }
 
 
+  int fastq_file_reader(FILE * fp, Sequence * seq, int max_read_length, boolean new_entry, boolean * full_entry){
+    * full_entry = true;
+
+    if (new_entry!= true){
+      puts("new_entry has to be true for fastq\n");
+      exit(1);
+    }
+
+    return read_sequence_from_fastq(fp,seq,max_read_length);
+  }
+  
+
+  int fasta_file_reader(FILE * fp, Sequence * seq, int max_read_length, boolean new_entry, boolean * full_entry){
+    long long ret;
+    int offset = 0;
+    if (new_entry == false){
+      offset = kmer_size;
+    }
+    ret =  read_sequence_from_fasta(fp,seq,max_read_length,new_entry,full_entry,offset);
+    
+    return ret;
+  }
 
 
+  
+  //action starts here
   kmer_size        = atoi(argv[2]);
   hash_key_bits    = atoi(argv[3]); //2 ** hash_key_bits buckets
+  bucket_size      = atoi(argv[4]);
+  fastq            = atoi(argv[5]);
 
-  fprintf(stderr,"Kmer size:%d hash_table_size (bits):%d\n",kmer_size,hash_key_bits);
+  if (kmer_size>31){
+    fprintf(stderr,"kmer size [%i] exceeds 31\n",kmer_size);
+    exit(1);
+  }
 
-  hash_table = hash_table_new(hash_key_bits,kmer_size);
+  fprintf(stderr,"Input file of filenames: %s\n",argv[1]);
+  fprintf(stderr,"Kmer size: %d hash_table_size (%d bits): %d - bucket size: %d - total size: %qd\n",kmer_size,hash_key_bits,1 << hash_key_bits, bucket_size, ((long long) 1<<hash_key_bits)*bucket_size);
 
-  fprintf(stderr,"hash table created with this many buckets: %i\n",1<<hash_key_bits);
+
+  hash_table = hash_table_new(hash_key_bits,bucket_size, 10,kmer_size); //10 is the number of rehashing attemps 
+  fprintf(stderr,"table created: %d\n",1 << hash_key_bits); 
+
+  if (fastq>0){
+    fprintf(stderr,"fastq files: quality cut-off: %i\n",fastq);
+  }
+  else{
+    fprintf(stderr,"fasta files\n");
+  }
 
   //open file of file names
   fp_fnames= fopen(argv[1], "r");
 
-  int count_kmer   = 0;
+
   int count_file   = 0;
   long long total_length = 0; //total sequence length
 
+
+  //----------------------------------
+  // preallocate the memory used to read the sequences
+  //----------------------------------
+  seq = malloc(sizeof(Sequence));
+  if (seq == NULL){
+    fputs("Out of memory trying to allocate Sequence\n",stderr);
+    exit(1);
+  }
+  alloc_sequence(seq,max_read_length,LINE_MAX);
+  
+  
+ 
+
+
+  //max_read_length/(kmer_size+1) is the worst case for the number of sliding windows, ie a kmer follow by a low-quality/bad base
+  int max_windows = max_read_length/(kmer_size+1);
+ 
+  //number of possible kmers in a 'perfect' read
+  int max_kmers   = max_read_length-kmer_size+1;
+
+  //----------------------------------
+  //preallocate the space of memory used to keep the sliding_windows. NB: this space of memory is reused for every call 
+  //-- with the view to avoid memory fragmentation
+  //NB: this space needs to preallocate memory for orthogonal situations: 
+  //    * a good read -> few windows, many kmers per window
+  //    * a bad read  -> many windows, few kmers per window    
+  //----------------------------------
+  
+  KmerSlidingWindowSet * windows = malloc(sizeof(KmerSlidingWindowSet));  
+  if (windows == NULL){
+    fputs("Out of memory trying to allocate a KmerArraySet",stderr);
+    exit(1);
+  }  
+  //allocate memory for the sliding windows 
+  binary_kmer_alloc_kmers_set(windows, max_windows, max_kmers);
+  
+
+  //for each file 
   while (!feof(fp_fnames)){
 
-    int count_bad_reads = 0;
+    long long count_bad_reads = 0;
     fscanf(fp_fnames, "%s\n", filename);
     
     fp_file = fopen(filename, "r");
@@ -68,34 +156,60 @@ int main(int argc, char **argv){
       exit(1);
     }
     
-    int seq_length = 0;
+    long long seq_length = 0;
     count_file++;
+    
+    while ((entry_length = fastq == 0 ? 
+			   fasta_file_reader(fp_file,seq,max_read_length,full_entry,&full_entry) :
+			   fastq_file_reader(fp_file,seq,max_read_length,full_entry,&full_entry))){
 
-    while ((seq = read_sequence_from_fasta(fp_file))){
-      KmerArray *kmers;
-      int i;
+      int i,j;
 
-      seq_length += seq->length;
+      seq_length += (long long) (entry_length - (prev_full_entry==false ? kmer_size : 0));
+   
+      int nkmers = get_sliding_windows_from_sequence(seq->seq,seq->qual,
+						     entry_length,fastq,kmer_size,windows,max_windows, max_kmers);
 
-      kmers = get_binary_kmers_from_sequence(seq->seq,seq->length,kmer_size);
-      free_sequence(&seq); 
-
-      if (kmers == NULL){
+      
+      if (nkmers == 0) {
 	count_bad_reads++;
       }
-      else{
-	
-	for(i=0;i<kmers->nkmers;i++){
-	  hash_table_apply_or_insert(element_get_key(kmers->bin_kmers[i],hash_table->kmer_size),&element_increment_count,hash_table);
+      else {
+
+	 for(i=0;i<windows->nwindows;i++){ //for each window
+	   KmerSlidingWindow * current_window = &(windows->window[i]);
+	   
+	   for(j=0;j<current_window->nkmers;j++){ //for each kmer in window
+	     boolean found = false;
+
+	     Element * current_entry = hash_table_find_or_insert(element_get_key(current_window->kmer[j],kmer_size),&found,hash_table);	
+ 
+	     if (current_entry == NULL){
+	       fputs("file_reader: problem - current kmer not found\n",stderr);
+	       exit(1);
+	     }
+	     
+	     if (! (i==0 && j==0 && prev_full_entry == false && current_entry == prev_entry)){ //otherwise is the same old last entry 
+	       element_increment_count(current_entry);
+	     }
 	  
+	   }
+	 }
       }
       
-	binary_kmer_free_kmers(&kmers);
+      //for long fasta entries this pushes the last kmer back to the beginning of the sequence
+      if (full_entry == false){
+	shift_last_kmer_to_start_of_sequence(seq,entry_length,kmer_size);
       }
-    }  
+      
+      prev_full_entry = full_entry;
+      
+    }
      
     total_length+=seq_length;
-    fprintf(stderr,"%i file name:%s kmers:%i bad_reads:%i seq:%i total seq:%qd\n",count_file,filename,count_kmer,count_bad_reads,seq_length, total_length);
+    
+    fprintf(stderr,"\n%i kmers: %qd file name:%s bad reads: %qd seq:%qd total seq:%qd\n\n",count_file,hash_table_get_unique_kmers(hash_table),filename,count_bad_reads,seq_length, total_length);
+
     
   }
 
