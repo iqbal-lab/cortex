1103b51b3db4c6f07b01b0e06c94e552f823274d
zam <zam@birch.well.ox.ac.uk>
2010-04-05 08:37:15 +0100

    Fix 2 small bugs in case 10. Namely:
    1. Did not close file pointer after hom non-ref bubble calls
    2. Did not clean status entries of hash table after printing na12878 supernodes,
       so all supernode printing after that did not work

--- src/pop_graph/sv_trio/graph.c
+++ src/pop_graph/sv_trio/graph.c
@@ -1104,7 +1104,7 @@ int main(int argc, char **argv){
 	db_graph_detect_vars( detect_vars_hom_nonref_fptr, max_allowed_branch_len,db_graph, &detect_vars_condition_is_hom_nonref_in_both_individuals,
 			      &db_node_action_set_status_visited,  &db_node_action_set_status_visited,
 			      &element_get_union_human_indiv_and_ref_colours, &get_joint_human_indiv_and_ref_covg, &print_extra_info);
-
+	fclose(detect_vars_hom_nonref_fptr);
 	//cleanup
 	hash_table_traverse(&db_node_action_set_status_none, db_graph);	
 
@@ -1198,12 +1198,15 @@ int main(int argc, char **argv){
 
 	printf("Now print annotated supernodes of  NA12878\n");
 	db_graph_print_supernodes_for_specific_person_or_pop("na12878_sups", "na12878_sings", 3000, db_graph, individual_edge_array,colour_na12878, &print_extra_supernode_info);
+	hash_table_traverse(&db_node_action_set_status_none, db_graph);	
 	printf("Now print annotated supernodes of  NA19240\n");
 	db_graph_print_supernodes_for_specific_person_or_pop("na19240_sups", "na19240_sings", 3000, db_graph, individual_edge_array,colour_na19240, &print_extra_supernode_info);
+	hash_table_traverse(&db_node_action_set_status_none, db_graph);	
 	printf("Now print annotated SHARED supernodes of  NA12878 and NA19240\n");
 	db_graph_print_supernodes_defined_by_func_of_colours("shared_sups", "shared_sings", 3000, 
 							     db_graph, &element_get_union_human_colours, &get_joint_human_covg,
 							     &print_extra_supernode_info);
+	hash_table_traverse(&db_node_action_set_status_none, db_graph);	
 
 	printf("FINISHED ALL\n");
 	break;
