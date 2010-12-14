36d0575ef6b134c2fc48c000c1462180d13b6a52
zam <zam@birch.well.ox.ac.uk>
2010-12-14 19:34:38 +0000

    Add GPL to cortex_var

--- include/test/cortex_var/many_colours/test_cmd_line.h
+++ include/test/cortex_var/many_colours/test_cmd_line.h
@@ -1,3 +1,29 @@
+/*
+ * 
+ * CORTEX project contacts:  
+ * 		M. Caccamo (mario.caccamo@bbsrc.ac.uk) and 
+ * 		Z. Iqbal (zam@well.ox.ac.uk)
+ *
+ * **********************************************************************
+ *
+ * This file is part of CORTEX.
+ *
+ * CORTEX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CORTEX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CORTEX.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * **********************************************************************
+ */
+
 void test_parse_colourinfo_argument();
 void test_parse_commasep_list();
 void test_get_numbers_from_comma_sep_list();
