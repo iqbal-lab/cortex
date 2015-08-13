37e147786e258e7835501c5fce3ec5d2eafa8c30
Zamin Iqbal <zam@well.ox.ac.uk>
2015-08-13 01:06:19 +0100

    Progress towards prepare.pl being ready

--- scripts/calling/BasicUtils.pm
+++ scripts/calling/BasicUtils.pm
@@ -19,85 +19,81 @@ sub add_slash
 
 sub is_fasta
 {
-	my ($file) = @_;
-
-		my $cmd = "head -n 3 $file\n";
-		my $ret = qx{$cmd};
-
-		my @lines = split("\n", $ret);
-		if (substr($lines[0], 0,1) ne ">")
-		{
-			return "EMissingGtrSign";
-		}
-		elsif ($lines[1] !~ /^[ACGTNacgtn]$/)
-		{
-			return "ENotBases";
-		}
-		elsif ($lines[2] =~ /+/)
-		{
-			return "EFastq";
-		}
-		elsif ((substr($lines[2]),0,1) ne ">"))
-		{
-			return "EMissingGtrSign";
-		}
-		else
-		{
-			return "EFasta";
-		}
+    my ($file) = @_;
+    
+    my $cmd = "head -n 3 $file\n";
+    my $ret = qx{$cmd};
+    
+    my @lines = split("\n", $ret);
+    if (substr($lines[0], 0,1) ne ">")
+    {
+	return "EMissingGtrSign";
+    }
+    elsif ($lines[1] !~ /^[ACGTNacgtn]+$/)
+    {
+	return "ENotBases";
+    }
+    elsif ($lines[2] =~ /\+/)
+    {
+	return "EFastq";
+    }
+    else
+    {
+	return "EFasta";
+    }
 }
 
 
 sub is_fastq
 {
-	my ($file) = @_;
-
-		my $cmd = "head -n 5 $file\n";
-		my $ret = qx{$cmd};
-
-		my @lines = split("\n", $ret);
-		if (substr($lines[0], 0,1) ne "@")
-		{
-			return "EMissingAtSign";
-		}
-		elsif ($lines[1] !~ /^[ACGTNacgtn]$/)
-		{
-			return "ENotBases";
-		}
-		elsif ($lines[2] !~ /+/)
-		{
-			return "ENotFastq";
-		}
-		elsif (length($lines[3]) != length($lines[0]))
-		{
-			return "EQualLineNotMatchSeqLineInFirstRead";
-		}
-		elsif (substr($lines[4], 0,1) ne "@")
-		{
-				return "ENotFastq";
-		}
-		else
-		{
-			return "EFastq";
-		}
+    my ($file) = @_;
+    
+    my $cmd = "head -n 5 $file\n";
+    my $ret = qx{$cmd};
+    
+    my @lines = split("\n", $ret);
+    if (substr($lines[0], 0,1) ne "@")
+    {
+	return "EMissingAtSign";
+    }
+    elsif ($lines[1] !~ /^[ACGTNacgtn]$/)
+    {
+	return "ENotBases";
+    }
+    elsif ($lines[2] !~ /[+]/)
+    {
+	return "ENotFastq";
+    }
+    elsif (length($lines[3]) != length($lines[0]))
+    {
+	return "EQualLineNotMatchSeqLineInFirstRead";
+    }
+    elsif (substr($lines[4], 0,1) ne "@")
+    {
+	return "ENotFastq";
+    }
+    else
+    {
+	return "EFastq";
+    }
 }
 
 sub create_dir_if_does_not_exist
 {
-	my ($dir, $funcname) = @_;
+    my ($dir, $funcname) = @_;
+    if (!(-d $dir))
+    {
+	my $cmd = "mkdir -p $dir";
+	my $ret = qx{$cmd};
+	
 	if (!(-d $dir))
-	{
-		my $cmd = "mkdir -p $dir";
-		my $ret = qx{$cmd};
-
-		if (!(-d $dir))
     	{
-        	my $str = "Unable to create directory ";
-        	$str .= "$loc_refdir in function $funcname\n";
-        	$str .= "Error output:\n$ret\n";
-        	die($str);
+	    my $str = "Unable to create directory ";
+	    $str .= "$dir in function $funcname\n";
+	    $str .= "Error output:\n$ret\n";
+	    die($str);
     	}	
-	}
+    }
 }
 
 sub count_bases_in_fasta
@@ -117,3 +113,5 @@ sub count_bases_in_fasta
 	close(FILE);
 	return $tot;
 }
+
+1;
--- scripts/calling/prepare.pl
+++ scripts/calling/prepare.pl
@@ -7,10 +7,20 @@ use Getopt::Long;
 use Benchmark;
 use Cwd    qw( abs_path );
 use FindBin qw($Bin);
-use BasicUtils qw ( add_slash, is_fastq, count_bases_in_fasta );
 
-my $cortex_dir = abs_path($0);
-$cortex_dir =~ s/scripts\/calling\/prepare.pl//;
+my $cortex_dir;
+
+BEGIN
+{
+    $cortex_dir = abs_path($0);
+    $cortex_dir =~ s/scripts\/calling\/prepare.pl//;
+
+    push ( @INC, $cortex_dir."scripts/calling/");
+}
+
+use BasicUtils qw ( add_slash is_fastq count_bases_in_fasta create_dir_if_does_not_exist );
+
+
 
 ## Script to prepare reference graph binary, Stampy hash of reference etc
 
@@ -31,42 +41,54 @@ my $ref_id = "REF";
     'dir_for_ref_objects:s'       =>\$refdir, 
     'vcftools_dir:s'              =>\$vcftools_dir,
     'outdir:s'                    =>\$outdir,
-    'stampy_bin :s'               =>\$stampy_bin,
-    'stampy_hash:s'               =>\$stampy_hash,
-    'ref_id:s'                    =>\$ref_id,
+    'stampy_bin:s'               =>\$stampy_bin,
+   # 'stampy_hash:s'               =>\$stampy_hash,
+    'ref_id:s'                    =>\$ref_id, ##not mandatory
     'kmer:i'                      =>\$kmer,
     );
 
 
-$outdir = add_slash($outdir);
 
 
 
 ## will make outdir if it does not exist, as well as refdir
 ## it returns absolute paths 
-my $num_samples, $mem_height, $mem_width;
-    ($num_samples, $mem_height, $mem_width, $genome_size,
-    $ref_fa, $stampy_bin, $stampy_hash, $outdir )  
+my $num_samples;
+my $mem_height;
+my $mem_width;
+($num_samples, $mem_height, $mem_width, $genome_size,
+ $ref_fa, $stampy_bin, $stampy_hash, $outdir )  
+    
+    ## includes creating missing dirs and checking Stampy, VCFtools
+    = check_args($all_samples_index, $ref_fa, $refdir, $vcftools_dir, $outdir, 
+		 $stampy_bin, $stampy_hash, $kmer, $ref_id);
 
-        ## includes creating missing dirs and checking Stampy, VCFtools
-        = check_args($index, $ref_fa, $refdir, $vcftools_dir, $outdir, 
-	   	       		 $stampy_bin, $stampy_hash, $kmer, $genome_size);
-
+$outdir = BasicUtils::add_slash($outdir);
 
 
 ##  Will save useful info ain a config file
 my $config = $outdir."config.prep.txt";
+my $c_fh;
 open ($c_fh, ">".$config)||die("Cannot create $config file - file permissions 
     issue?\n");
 
 ##Variables which I will write to the config file
-my @arr_config = ($ref_fa, $refdir, $vcftools_dir,
-                $stampy_bin, $stampy_hash, $genome_size);
-add_to_config($c_fh, \@arr_config);
+my %arr_config = ("ref_fa" => $ref_fa, 
+		  "refbindir"=> $refdir."/ctx_bins", 
+		  "vcftools_dir"=>$vcftools_dir,
+		  "stampy_bin"=>$stampy_bin, 
+		  #"stampy_hash"=>$stampy_hash, 
+		  "genome_size"=>$genome_size);
 
+#add_to_config($c_fh, \%arr_config);
+
+
+my $cmd = "export LD_LIBRARY_PATH=$cortex_dir"."/libs/gsl-1.15/:$cortex_dir"."/libs/gsl-1.15/blas/:$cortex_dir"."/libs/gsl-1.15/.libs/:$cortex_dir"."/libs/gsl-1.15/blas/.libs/:$cortex_dir"."/libs/gsl-1.15/cblas/.libs/:\$LD_LIBRARY_PATH";
+qx{$cmd};
 
 ### Compile Cortex
-my $cortex_bin = compile_cortex($cortex_dir, $num_samples);
+my $cortex_bin = compile_cortex($cortex_dir, $num_samples, $kmer);
+
 ### Check can run Cortex
 check_cortex_runnable($cortex_bin);
 
@@ -96,8 +118,11 @@ else
 {
     print "reference binary $refbin already exists\n";
 }
-@arr_config = ($ref_falist, $ref_fa, $refbin);
-add_to_config($c_fh, \@arr_config);
+
+$arr_config{"ref_falist"}=$ref_falist;
+$arr_config{"ref_fa"} =$ref_fa;
+$arr_config{"refbin"}= $refbin;
+#add_to_config($c_fh, \@arr_config);
 
 
 ### Build stampy hash
@@ -108,7 +133,10 @@ my $cmd_stampy2 = "$stampy_bin -g $stampy_stub -H $stampy_stub";
 my $ret_stampy2 = qx{$cmd_stampy2};
 print "$cmd_stampy1\n$ret_stampy1\n$cmd_stampy2\n$ret_stampy2\n";
 
+$arr_config{"stampy_hash"}= $stampy_hash;
 
+add_to_config($c_fh, \%arr_config);
+close($c_fh);
 
 ###################################################################
 
@@ -117,140 +145,196 @@ sub check_args
 {
     my ($loc_index, $loc_ref_fa, $loc_refdir, $loc_vcftools_dir, 
         $loc_outdir, $loc_stampy_bin, $loc_stampy_hash, 
-        $loc_kmer) = @_;
-
-if (($loc_index eq "") || (!(-e $loc_index)))
-{
-    my $str = "You must specify an index file with --index.\n";
-    $str .= "Each sample corresponds to one line of this file.\n";
-    $str .= "Each line is tab separated with 4 fields."
-    $str .= "Field1 = sample identifier\n";
-    $str .= "Field2 = full path to a filelLIST, listing all fastq or bams";
-    $str .= "         for this sample. Do NOT put the name of an actual";
-    $str .= "         fastq/bam in this field - it expects a LIST.\n";
-    $str .= "         These sequence will be treated as single-ended, but\n";
-    $str .= "         Cortex only uses paired-end info to remove PCR \n";
-    $str .= "         duplicates.\n"
-    $str .= "Field3 = As field 2, for left-hand reads from paired ends\n";
-    $str .= "         You can just put a dot \".\" to ignore this.\n";
-    $str .= "Field3 = As field 3, for right-hand reads from paired ends\n";
-    $str .= "         You can just put a dot \".\" to ignore this.\n";
-    die("Please create this file and specify it using --index\n");
-}
-my $num_samples_cmd = "wc -l $loc_index";
-my $num_samples = qx{$num_samples_cmd};
-chomp $num_samples;
-if ($num_samples =~ /^(\d+)/)
-{
-    $num_samples = $1;
-}
-else
-{
-    die("Failed to count lines in index $loc_index - permissions issue?\n");
-}
-
-if (!(-e $loc_ref_fa))
-{
-    die("Unable to open your specified reference fasta $loc_ref_fa\n")
-}
-elsif (is_fasta($loc_ref_fa) ne "EFasta")
-{
-    my $str = "Your specified reference fasta file $loc_ref_fa appears ";
-    $str .= "not to be in fasta format\n";
-    die($str);
-}
-
-
-
+        $loc_kmer, $loc_refid) = @_;
+    
+    if (($loc_index eq "") || (!(-e $loc_index)))
+    {
+	my $str = "You must specify an index file with --index.\n";
+	$str .= "Each sample corresponds to one line of this file.\n";
+	$str .= "Each line is tab separated with 4 fields.";
+	$str .= "Field1 = sample identifier\n";
+	$str .= "Field2 = full path to a filelLIST, listing all fastq or bams";
+	$str .= "         for this sample. Do NOT put the name of an actual";
+	$str .= "         fastq/bam in this field - it expects a LIST.\n";
+	$str .= "         These sequence will be treated as single-ended, but\n";
+	$str .= "         Cortex only uses paired-end info to remove PCR \n";
+	$str .= "         duplicates.\n";
+	$str .= "Field3 = As field 2, for left-hand reads from paired ends\n";
+	$str .= "         You can just put a dot \".\" to ignore this.\n";
+	$str .= "Field3 = As field 3, for right-hand reads from paired ends\n";
+	$str .= "         You can just put a dot \".\" to ignore this.\n";
+	die("Please create this file and specify it using --index\n");
+    }
+    my $num_samples_cmd = "wc -l $loc_index";
+    my $num_samples = qx{$num_samples_cmd};
+    chomp $num_samples;
+    if ($num_samples =~ /^(\d+)/)
+    {
+	$num_samples = $1;
+    }
+    else
+    {
+	die("Failed to count lines in index $loc_index - permissions issue?\n");
+    }
+    
+    if (!(-e $loc_ref_fa))
+    {
+	die("Unable to open your specified reference fasta $loc_ref_fa\n")
+    }
+    else 
+    {
+	my $err = BasicUtils::is_fasta($loc_ref_fa);
+	if ($err ne "EFasta")
+	{
+	    my $str = "Your specified reference fasta file $loc_ref_fa appears ";
+	    $str .= "not to be in fasta format\n";
+	    die($str."   ".$err);
+	}
+    }
+    
+    
+    
 ### Calculate genome size and memory parameters
-my $genome_len = count_bases_in_fasta($loc_ref_fa);
-
-my $mem_w=100;
+    my $genome_len = BasicUtils::count_bases_in_fasta($loc_ref_fa);
+    
+    my $mem_w=100;
 ## 2^height * width = 3g
 ## => height = log_2(3g/width)
-my $mem_h = int(log(1.5*$genome_len/100)/log(2) +0.5);
-
-
+    my $mem_h = int(log(1.5*$genome_len/100)/log(2) +0.5);
+    
+    
 ## Set up the reference directory
-create_dir_if_does_not_exist($loc_refdir, "check_args of prepare.pl");
-$loc_refir = add_slash($loc_refdir);
-my $st_dir = $loc_refdir."stampy/";
-my $ctxdir = $loc_refdir."ctx_bins/";
-create_dir_if_does_not_exist($st_dir,  "check_args of prepare.pl");
-create_dir_if_does_not_exist($ctxdir, "check_args of prepare.pl");
-
-
+    BasicUtils::create_dir_if_does_not_exist($loc_refdir, "check_args of prepare.pl");
+    $loc_refdir = BasicUtils::add_slash($loc_refdir);
+    my $st_dir = $loc_refdir."stampy/";
+    my $ctxdir = $loc_refdir."ctx_bins/";
+    BasicUtils::create_dir_if_does_not_exist($st_dir,  "check_args of prepare.pl");
+    BasicUtils::create_dir_if_does_not_exist($ctxdir, "check_args of prepare.pl");
+    
+    
 ### is the VCFtools directory actualy a directory with the right stuff?
-if (!(-d $loc_vcftools_dir))
-{
-    die("The vcftools directory specified ($loc_vcftools_dir) doesn't exist\n");
-}
-else
-{
-    $loc_vcftools_dir=add_slash($loc_vcftools_dir);
-    my $d1 = $loc_vcftools_dir."perl";
-    if (!(-e $d1))
+    if (!(-d $loc_vcftools_dir))
     {
-        my $str = "The specified vcftools_dir is not actually the directory ";
-        $str .= "of a full VCFtools install/download. At the least it should ";
-        $str .= " contain the perl/ directory that there when you download a ";
-        $str .= " .tgz file of VCFtools\n";
-        die($str);
+	die("The vcftools directory specified ($loc_vcftools_dir) doesn't exist\n");
     }
-}
-
-
+    else
+    {
+	$loc_vcftools_dir=BasicUtils::add_slash($loc_vcftools_dir);
+	my $d1 = $loc_vcftools_dir."perl";
+	if (!(-e $d1))
+	{
+	    my $str = "The specified vcftools_dir is not actually the directory ";
+	    $str .= "of a full VCFtools install/download. At the least it should ";
+	    $str .= " contain the perl/ directory that there when you download a ";
+	    $str .= " .tgz file of VCFtools\n";
+	    die($str);
+	}
+    }
+    
+    
 ## Sort out the output directory
-create_dir_if_does_not_exist($loc_outdir, "check_args of prepare.pl");
-$loc_outdir = add_slash($loc_outdir);
-my $filelist_dir = $loc_outdir."filelists/";
-create_dir_if_does_not_exist($filelist_dir,  "check_args of prepare.pl");
-
+    BasicUtils::create_dir_if_does_not_exist($loc_outdir, "check_args of prepare.pl");
+    $loc_outdir = BasicUtils::add_slash($loc_outdir);
+    my $filelist_dir = $loc_outdir."filelists/";
+    BasicUtils::create_dir_if_does_not_exist($filelist_dir,  "check_args of prepare.pl");
+    
 ## Create the reference fasta filelist
-my $ref_fa_list = $filelist_dir."list_ref_fa";
-my $cmd_create_reffalist = "ls $loc_ref_fa > $ref_fa_list";
-qx{$cmd_create_reffalist};
-
-
+    my $ref_fa_list = $filelist_dir."list_ref_fa";
+    my $cmd_create_reffalist = "ls $loc_ref_fa > $ref_fa_list";
+    qx{$cmd_create_reffalist};
+    
+    
 ## Check the stampy binary
-if (!(-e $loc_stampy_bin))
-{
-    die("You must specify the full path to stampy.py using --stampy_bin\n");
-}
-elsif ($loc_stampy_bin !~ /stampy.py$/)
-{
-    die("--stampy_bin shoould give full path to stampy.py\n")
-}
-my $stcmd = "$stampy_bin --help";
-my $stret = qx{$stcmd};
-
-if ($stret !~ /Usage/)
-{
+    if (!(-e $loc_stampy_bin))
+    {
+	die("You must specify the full path to stampy.py using --stampy_bin\n");
+    }
+    elsif ($loc_stampy_bin !~ /stampy.py$/)
+    {
+	die("--stampy_bin shoould give full path to stampy.py\n")
+    }
+    my $stcmd = "$stampy_bin --help";
+    my $stret = qx{$stcmd};
+    
+    if ($stret !~ /Usage/)
+    {
         my $str = "The stampy.py specified does not seem to run (tried "; 
         $str .= " calling --help. Maybe Python 2.6 or 2.7 not present?\n";
         die($str);
-}
-
-my $stf1 = $stampy_hash.".stidx";
-my $stf2 = $stampy_hash.".sthash";
-if ( (!(-e $stf1)) || (!(-e $stf2)) )
-{
-    ##They do not both exist
-
-    if (-e $stf1) 
-    {
-        my $c1 ="rm $stf1";
-        qx{$c1};
     }
-    if (!( -e $stf2))
+    
+    if ($loc_stampy_hash eq "")
     {
-        my $c2 ="rm $stf2";
-        qx{$c2};
+	$loc_stampy_hash=$loc_refdir."stampy/REF";
+    }
+    my $stf1 = $loc_stampy_hash.".stidx";
+    my $stf2 = $loc_stampy_hash.".sthash";
+    if ( (!(-e $stf1)) || (!(-e $stf2)) )
+    {
+	##They do not both exist
+	
+	if (-e $stf1) 
+	{
+	    my $c1 ="rm $stf1";
+		qx{$c1};
+	}
+	if (!( -e $stf2))
+	    {
+		my $c2 ="rm $stf2";
+		qx{$c2};
+	    }
+    }
+    
+    if ($loc_kmer % 2==0)
+    {
+	die("Kmer must be an odd number\n");
+    }	
+    
+    return ($num_samples, $mem_h, $mem_w, $genome_len,
+	    $loc_ref_fa, $loc_stampy_bin, $loc_stampy_hash, $loc_outdir )    
+}
+
+sub compile_cortex
+{
+    my ($ctx_dir, $n_samples, $k) = @_;
+
+    my $maxk = 32 * (int($k / 32) +1) -1;
+
+    my $c = "cd $ctx_dir; source setenv.sh; make NUM_COLS=$n_samples MAXK=$maxk cortex_var;";
+    qx{$c};
+    $ctx_dir = BasicUtils::add_slash($ctx_dir);
+    my $bin = $ctx_dir."bin/cortex_var_".$maxk."_c".$n_samples;
+    if (!(-e $bin))
+    {
+	die("Failed to compile Cortex, tried to make $bin and failed\n");
+    }
+    return $bin;
+}
+
+sub check_cortex_runnable
+{
+    my ($ctx_bin) = @_;
+    
+    my $cmd = "$ctx_bin --help";
+    my $ret = qx{$cmd};
+    if ($ret =~ /Starting Cortex/)
+    {
+	return; 
+    }
+    else
+    {
+	die("prepare.pl has compiled Cortex, but it cannot run the executable/binary. This probably means some library (probably GSL) could not be find\n");
     }
 }
 
-if ($loc_kmer % 2==0)
-{
-    die("Kmer must be an odd number\n");
-}
 
+sub add_to_config
+{
+    my ($fh, $href) = @_;
+
+    foreach my $key (keys %$href)
+    {
+	print $fh $key."\t".$href->{$key};
+	print $fh "\n";
+    }
+}
