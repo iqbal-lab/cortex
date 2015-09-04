8c7fd20ee65226cf403278cd003bfc52b2a67a8d
Zamin Iqbal <zam@well.ox.ac.uk>
2015-09-05 00:23:24 +0100

    Fix bug in run_indep... This commit leaves initial steps commented out for testing

--- scripts/calling/run_indep_wkflow_with_gnu_par.pl
+++ scripts/calling/run_indep_wkflow_with_gnu_par.pl
@@ -57,7 +57,7 @@ my $mem_width="";
 my $prep = "perl $cortex_dir"."scripts/calling/prepare.pl --index $all_samples_index --ref_fa $ref_fa";
 $prep .= " --dir_for_ref_objects $refdir --vcftools_dir $vcftools_dir --outdir $outdir ";
 $prep .= " --stampy_bin $stampy_bin --kmer $kmer";
-my $ret_prep = qx{$prep};
+#my $ret_prep = qx{$prep};
 
 ## Build
 
@@ -80,23 +80,23 @@ if ( ($mem_height ne "") && ($mem_width ne ""))
 }
 $build .= " ::: {1..".$num_samples."}";
 
-my $ret_build = qx{$build};
+#my $ret_build = qx{$build};
 
 
 ## Combine
 
 my $combine = "perl $cortex_dir"."scripts/analyse_variants/combine/combine_vcfs.pl ";
 $combine .= " --prefix $prefix --outdir $outdir --intersect_ref ";
-my $ret_combine = qx{$combine};
-print $ret_combine;
+#my $ret_combine = qx{$combine};
+#print $ret_combine;
 
 ## Genotype (in parallel)
 
 my $gt = "cat $outdir"."combine/list_args_for_final_step | ";
 $gt .= "parallel --gnu -j $num_procs --colsep \'\\t\' ";
 $gt .= $cortex_dir."scripts/calling/gt_1sample.pl ";
-$gt .= $outdir."combine/config.txt ";
-$gt .= $outdir."/combine/".$prefix."_sites.vcf ";
+$gt .= "--config ".$outdir."combine/config.txt ";
+$gt .= "--invcf ".$outdir."/combine/".$prefix.".sites_vcf ";
 $gt .= " --sample {1} --outdir {2} --sample_graph {3} ";
 print "$gt\n";
 my $ret_gt = qx{$gt};
