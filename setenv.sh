3e716b22d41a7713db83f5d4ad36694e03a1b8b7
Zamin Iqbal <zam@well.ox.ac.uk>
2015-11-11 00:39:50 +0000

    Remove legacy files

--- setenv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-CURRENT_DIR=$PWD
-export LD_LIBRARY_PATH=$CURRENT_DIR/libs/gsl-1.15/:$CURRENT_DIR/libs/gsl-1.15/blas/:$CURRENT_DIR/libs/gsl-1.15/.libs/:$CURRENT_DIR/libs/gsl-1.15/blas/.libs/:$CURRENT_DIR/libs/gsl-1.15/cblas/.libs/:$LD_LIBRARY_PATH
